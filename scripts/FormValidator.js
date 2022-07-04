ad358e5446b9726a98f9ab4cfaff78a82fc7d316
Andrey Amelin <more207@mail.ru>
2022-07-04 20:49:02 +0300

    fix: добавить метод resetValidation

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -2,23 +2,31 @@ export default class FormValidator {
   constructor(selectorsNames, formElement) {
     this._formElement = formElement;
     this._selectorsNames = selectorsNames;
-    this._buttonElement = this._formElement.querySelector(this._selectorsNames.submitButtonSelector);
-    this._inputList = Array.from(formElement.querySelectorAll(this._selectorsNames.inputSelector));
-  };
+    this._buttonElement = this._formElement.querySelector(
+      this._selectorsNames.submitButtonSelector
+    );
+    this._inputList = Array.from(
+      formElement.querySelectorAll(this._selectorsNames.inputSelector)
+    );
+  }
 
   _showInputError(inputElement, errorMessage) {
-    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.id}-error`
+    );
     inputElement.classList.add(this._selectorsNames.inputErrorClass);
     errorElement.classList.add(this._selectorsNames.errorClass);
     errorElement.textContent = errorMessage;
-  };
+  }
 
   _hideInputError(inputElement) {
-    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.id}-error`
+    );
     inputElement.classList.remove(this._selectorsNames.inputErrorClass);
     errorElement.classList.remove(this._selectorsNames.errorClass);
-    errorElement.textContent = '';
-  };
+    errorElement.textContent = "";
+  }
 
   _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
@@ -26,42 +34,52 @@ export default class FormValidator {
     } else {
       this._hideInputError(inputElement);
     }
-  };
+  }
 
   _hasInvalidInput() {
     return this._inputList.some((item) => {
       return !item.validity.valid;
     });
-  };
+  }
 
-  disabledButton() {
+  _disabledButton() {
     this._buttonElement.classList.add(this._selectorsNames.inactiveButtonClass);
     this._buttonElement.disabled = true;
-  };
+  }
 
   _activeButton() {
-    this._buttonElement.classList.remove(this._selectorsNames.inactiveButtonClass);
+    this._buttonElement.classList.remove(
+      this._selectorsNames.inactiveButtonClass
+    );
     this._buttonElement.disabled = false;
-  };
+  }
 
   _toggleButtonState() {
     if (this._hasInvalidInput()) {
-      this.disabledButton();
+      this._disabledButton();
     } else {
       this._activeButton();
-    };
-  };
+    }
+  }
+
+  resetValidation() {
+    this._toggleButtonState();
+
+    this._inputList.forEach((item) => {
+      this._hideInputError(item);
+    });
+  }
 
   _setEventListeners() {
     this._inputList.forEach((inputElement) => {
-      inputElement.addEventListener('input', () => {
+      inputElement.addEventListener("input", () => {
         this._checkInputValidity(inputElement);
         this._toggleButtonState();
       });
     });
-  };
+  }
 
   enableValidation() {
     this._setEventListeners();
-  };
-};
+  }
+}
--- scripts/card.js
+++ scripts/card.js
@@ -6,51 +6,54 @@ export default class Card {
     this._imageLink = data.link;
     this._cardSelector = cardSelector;
     this._element = this._getTemplate();
-  };
+  }
 
   _getTemplate() {
     const cardElement = document
       .querySelector(this._cardSelector)
-      .content
-      .querySelector('.element')
+      .content.querySelector(".element")
       .cloneNode(true);
 
     return cardElement;
-  };
+  }
 
   _like(evt) {
-    evt.target.classList.toggle('element__like_active');
-  };
+    evt.target.classList.toggle("element__like_active");
+  }
 
   _removeCard(evt) {
-    evt.target.closest('.element').remove();
-  };
+    evt.target.closest(".element").remove();
+  }
 
   _scaleImage(evt) {
-    const popupForScaleImg = document.querySelector('.popup_for_scale-image');
-    const formImg = document.querySelector('.popup__image');
-    const formImgTitle = document.querySelector('.popup__image-title');
+    const popupForScaleImg = document.querySelector(".popup_for_scale-image");
+    const formImg = document.querySelector(".popup__image");
+    const formImgTitle = document.querySelector(".popup__image-title");
     formImg.src = evt.target.src;
     formImg.alt = evt.target.alt;
     formImgTitle.textContent = evt.target.alt;
 
     return openPopup(popupForScaleImg);
-  };
+  }
 
   _setEventListener() {
-    this._image = this._element.querySelector('.element__image');
-    this._element.querySelector('.element__like').addEventListener('click', this._like);
-    this._element.querySelector('.element__delete').addEventListener('click', this._removeCard);
-    this._image.addEventListener('click', this._scaleImage);
-  };
+    this._image = this._element.querySelector(".element__image");
+    this._element
+      .querySelector(".element__like")
+      .addEventListener("click", this._like);
+    this._element
+      .querySelector(".element__delete")
+      .addEventListener("click", this._removeCard);
+    this._image.addEventListener("click", this._scaleImage);
+  }
 
   generateCard() {
     this._setEventListener();
 
     this._image.src = this._imageLink;
     this._image.alt = this._title;
-    this._element.querySelector('.element__title').textContent = this._title;
+    this._element.querySelector(".element__title").textContent = this._title;
 
     return this._element;
-  };
-};
+  }
+}
